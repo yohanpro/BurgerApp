e1415c893776d98c3025fe607e2ebc80123c7c4f
ImJohnKim <yohan@yohanpro.com>
2018-06-20 19:28:45 +0900

    Burger Component with Burgerbuilder

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Aux from '../../hoc/Aux';
+import Burger from '../../components/Burger/Burger';
+
+class BurgerBuilder extends React.Component {
+    render() {
+        return (
+            <Aux>
+                <Burger />
+            </Aux>
+        );
+    }
+}
+
+export default BurgerBuilder ;
